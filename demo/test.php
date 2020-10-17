73314884a7065c4656569609c5331c953e852306
TANNER <2303386665@qq.com>
2020-10-17 17:53:35 +0800

    phpunit

---
+++ demo/test.php
@@ -0,0 +1,3 @@
+<?php
+$uniqid = uniqid();
+echo $uniqid;
---
+++ test/StackTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+	protected $stack;
+
+	protected function setUp(): void
+    {
+        $this->stack = [];
+    }
+
+    
+}
