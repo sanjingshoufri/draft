15a70521208f81928442fc2b25243018754d88e1
TANNER <2303386665@qq.com>
2020-10-22 18:05:34 +0800

    coroutine

--- demo/test.php
+++ demo/test.php
@@ -1,3 +1,26 @@
 <?php
-$uniqid = uniqid();
-echo $uniqid;
+// function gen()
+// {
+// 	for ($i = 0; $i < 2; $i++) { 
+// 		$a = yield;
+// 		echo $a . "<br>";
+// 	}
+// }
+
+// $iterator = gen();
+// $iterator->send("hello world");
+// $iterator->send("taonian");
+
+function gen() {
+    yield 'foo';
+    yield 'bar';
+}
+
+$gen = gen();
+// var_dump($gen->send('something'));
+
+$queue = new SplQueue();
+$queue->enqueue("taonian");
+
+$ret = $queue->dequeue();
+var_dump($ret);
---
+++ test/SelfTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class SelfTest extends TestCase
+{
+    public function testTn()
+    {
+        $this->markTestIncomplete('这个测试还没有完成...');
+    }
+}
