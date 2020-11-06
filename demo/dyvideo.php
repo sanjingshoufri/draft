5332450012bff9c2ce1148b0b09543e6226a0261
TANNER <2303386665@qq.com>
2020-11-06 18:35:23 +0800

    加入前端模板

--- demo/dyvideo.php
+++ demo/dyvideo.php
@@ -4,11 +4,18 @@
  * $share_url为抖音分享链接
  */ 
 
+$share_url = "TANNER:
+姨妈痛到吐我没哭 定闹钟抢裙子没抢到我没哭 你跟别人说我是你前任我哭了 我丢不起这个人  https://v.douyin.com/Jm3PtqF/ 复制此链接，打开抖音，直接观看视频！
+
+TANNER:
+  https://v.douyin.com/Jm3vV3W/ 复制此链接，打开抖音，直接观看视频！
+"; 
+
 // 从字符串中提取url.
 preg_match_all("/(https:\/\/)[a-z.\/0-9A-Z]+/", $share_url, $match);
 $urlList = $match[0];
 
-$base_url = "D:/taonian";
+$base_url = "E:/taonian";
 $dy_api = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=";
 
 // 批量下载
@@ -29,7 +36,10 @@ foreach ($urlList as $key => $url) {
     $arr = json_decode($ret, TRUE);
 
     $video_url = $arr['item_list'][0]['video']['play_addr']['url_list'][0];
-    $file_content = file_get_contents($video_url);
 
-    file_put_contents($base_url . "/" . $key . '.mp4', $file_content);
+    // 读取内容
+    $file_content = file_get_contents($video_url);
+    // 写入内容到磁盘
+    $file_name = $base_url . "/" . $key . time() . '.mp4';
+    file_put_contents($file_name, $file_content);
 }
--- demo/test.php
+++ demo/test.php
@@ -1,19 +1,3 @@
 <?php
-// 正则表达式测试
-$name = "杭州市浙江省测试自治区";
-$fir = preg_filter("/(省|市|自治区|特别行政区)/", "", $name);
-// var_dump($fir);
-// exit();
-
-
-
-$pattern = "/\b[A-Za-z]+/";
-$str = "taonian";
-preg_match($pattern, $str, $match);
-
-
-
-// $pattern = "/(杭州)/";
-// $name = "杭州市浙江省测试自治区";
-// $fir = preg_filter($pattern, "", $name);
-// var_dump($fir);
+// 
+echo '123';
